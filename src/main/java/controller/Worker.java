9898da598b68fb0ffc05f1e8036e41e9120743cf
zanghuaqian <zanghuaqian@weshare.com.cn>
2016-06-14 11:30:05 +0800

    RC-000: add work queue

--- src/main/java/controller/Worker.java
+++ src/main/java/controller/Worker.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -18,37 +19,42 @@ public class Worker {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		final Channel channel = connection.createChannel();
+		
+		boolean durable = true;
+		channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 		Consumer consumer = new DefaultConsumer(channel) {
 			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
 					throws IOException {
 				String message = new String(body, "UTF-8");
 				System.out.println(" [x] Recived '" + message + "'");
 				try {
 					doWork(message);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
 				} finally {
 					System.out.println(" [x] Done");
+					channel.basicAck(envelope.getDeliveryTag(), false);
 				}
 			}
 
 		};
-		channel.basicConsume(QUEUE_NAME, true, consumer);
+		boolean autoAck = false;
+		channel.basicConsume(QUEUE_NAME, autoAck, consumer);
 
 		channel.close();
 		connection.close();
 
 	}
 
-	private static void doWork(String task) throws InterruptedException {
+	private static void doWork(String task){
 		for (char ch : task.toCharArray()) {
 			if (ch == '.') {
-				Thread.sleep(1000);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
